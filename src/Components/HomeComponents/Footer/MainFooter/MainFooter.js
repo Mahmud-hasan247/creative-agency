1c6866089fc00bc8837d3e5fcff4c98eaa5fda74
Mahmud-hasan247 <mahmudhasan5406@gmail.com>
2020-11-09 17:20:09 +0600

    some changes

--- src/Components/HomeComponents/Footer/MainFooter/MainFooter.js
+++ src/Components/HomeComponents/Footer/MainFooter/MainFooter.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import './MainFooter.css';
+import emailjs from 'emailjs-com';
 
 const MainFooter = () => {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('creativeAgency', 'template_6jvi144', e.target, 'user_cvWtvUEiHYntwE4izcI3E')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        alert(`Thanks for messaging us. We'll reply to you as soon as possible.`);
+        e.target.reset();
+    }
+
     return (
         <>
             <div className='row'>
@@ -10,11 +25,11 @@ const MainFooter = () => {
                     <p className='pb-5'>Consectetur adipisicing elit. Sequi rem quisquam aspernatur, doloribus minus quibusdam. At quos adipisci asperiores eos voluptates dicta voluptatibus ipsum placeat ratione, aperiam, vitae quasi dolore!</p>
                 </div>
                 <div className="col-md-6 my-font mt-5 pt-5">
-                    <form action="">
-                        <input className="contact" placeholder="Your email address" type="email" /><br /><br />
-                        <input className="contact" placeholder="Your name / company name" type="text" /><br /><br />
-                        <input className="contact" placeholder='Your massage' id='massage' type="text" /><br /><br />
-                        <button className='btn btn-dark my-button'>Send</button>
+                    <form onSubmit={sendEmail}>
+                        <input className="contact" placeholder="Your email address" type="email" name='email' /><br /><br />
+                        <input className="contact" placeholder="Your name / company name" type="text" name='name' /><br /><br />
+                        <input className="contact" placeholder='Your massage' id='massage' type="text" name='message' /><br /><br />
+                        <input type="submit" className='btn btn-dark my-button' value='Send'/>
                     </form>
                 </div>
             </div>
